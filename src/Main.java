162decdd3e2240efcc5d28d9cbec1174e1686871
satyam262005 <satyam.cs241291@global.org.in>
2025-11-03 12:41:30 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Setup Successful");
+    }
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/satyam262005/java.git
+git push -u origin main
