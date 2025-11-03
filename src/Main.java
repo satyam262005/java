a0d028237d6128def17cf5f00515d345b17c0e82
satyam262005 <satyam.cs241291@global.org.in>
2025-11-03 13:14:40 +0530

    even,odd

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-class main{
-    public static void main(String[] args){
-        short b=5;
-        int c=5;
-        float f=90.3f;
-        boolean data=false;
+class main {
+    public static void main(String[] args) {
+        System.out.println("k");
+        {
+            short b = 5;
+            int c = 5;
+            float f = 90.3f;
+            boolean data = false;
 
-       System.out.println(b);
-        System.out.println(c);
-        System.out.println(f);
-        System.out.println(data);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(f);
+            System.out.println(data);
+        }
+        int a = 5;
+        if (a % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
     }
 }
