dea64da42635804d3a3f1cfdbd9adc5981c42a90
satyam262005 <satyam.cs241291@global.org.in>
2025-11-04 12:42:55 +0530

    string value

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,9 @@
+import java.util.Scanner;
 class main {
     public static void main(String[] args) {
-        System.out.println("k");
-        {
-            short b = 5;
-            int c = 5;
-            float f = 90.3f;
-            boolean data = false;
-
-            System.out.println(b);
-            System.out.println(c);
-            System.out.println(f);
-            System.out.println(data);
-        }
-        int a = 5;
-        if (a % 2 == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
-        }
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter you String value");
+String str= sc.nextLine();
+System.out.println(str);
     }
 }
