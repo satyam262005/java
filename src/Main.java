5e1f5355ba098dbe3151b9e7a8a529e1209f71d2
satyam262005 <satyam.cs241291@global.org.in>
2025-11-10 12:38:44 +0530

    string12

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,8 @@
 import java.util.Scanner;
 class main {
     public static void main(String[] args) {
-//       String str="hello java";
-//       System.out.println(str.length());
-//        System.out.println(str.charAt());
-//        System.out.println(str.toUpperCase());
-//        System.out.println(str.toLowerCase());
-//        System.out.println(str.indexOf());
-//        System.out.println(str.substring());
-//        System.out.println(str.substring());
-//        System.out.println(str.length());
-//        System.out.println(str.trim());
-        String str="hello";
-        int v=0;
-        for (char ch :str.toCharArray()){
-            if ("aeuio".indexOf(ch)!=-1){
-                v++;
-            }
-        }
-        System.out.println(v);
+        StringBuilder str=new StringBuilder("hello");
+        str.insert(2, "byee");
+        System.out.println(str);
     }
 }
